2554fe709dd46624a88e0a13414995d0710acfc9
Alisson Vinícius <alissonvincns@gmail.com>
2026-05-13 13:10:34 -0300

    implementações
    Implementei a página de visualizar multas do leitor

--- src/layouts/Sidebar.jsx
+++ src/layouts/Sidebar.jsx
@@ -5,17 +5,31 @@ import logo from '../assets/BibliotecaLogoBranco.png'
 function Sidebar(){
     return(
         <aside className={styles.principal}>
-            <img src={logo} alt="BibliotecaLogoBranco"  style={{ width: 250, marginBottom: 70 }}/>
+            <img src={logo} alt="BibliotecaLogoBranco"  style={{ width: 250, marginBottom: 30 }}/>
             <nav className={styles.nav}>
                 <ul className={styles.ul}>
                     <li className={styles.li}>Dashboard</li>
-                    <li className={styles.li}>
-                        <NavLink to="/bibliotecario/adicionar">Acervo</NavLink></li>
-                    <li className={styles.li}>
-                        <NavLink to="/bibliotecario/adicionarLeitor">Leitores</NavLink></li>
-                    <li className={styles.li}>
-                        <NavLink to="/bibliotecario/adicionarDevolucao">Empréstimos</NavLink></li>
+
+                    <NavLink to="/bibliotecario/adicionar">
+                        <li className={styles.li}>Acervo</li>
+                    </NavLink>
+
+                    <NavLink to="/bibliotecario/adicionarLeitor">
+                        <li className={styles.li}>Leitores</li>
+                    </NavLink>
+
+                    <NavLink to="/bibliotecario/adicionarDevolucao">
+                        <li className={styles.li}>Empréstimos</li>
+                    </NavLink>
+
+                    <NavLink to="/bibliotecario/pagarMulta">{/*------------------Teste(será removido dps)-----------------*/}
+                        <li className={styles.li}>Multa do leitor(teste)</li>
+                    </NavLink>
+                    
+
                     <li className={styles.li}>Relatórios</li>
+
+                    <li className={styles.li}>Sair</li>
                 </ul>
             </nav>
         </aside>
--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -4,6 +4,7 @@ import Login from '../pages/Login'
 import AdicionarItem from '../pages/AdicionarItem'
 import CadastrarLeitor from '../pages/CadastrarLeitor'
 import Devolucao from '../pages/Devolucao'
+import MultasDoLeitor from '../pages/MultasDoLeitor'
 
 export default function AppRoutes() {
   return (
@@ -16,6 +17,7 @@ export default function AppRoutes() {
             <Route path="adicionar" element={<AdicionarItem />} />
             <Route path="adicionarLeitor" element={<CadastrarLeitor />}></Route>
             <Route path="adicionarDevolucao" element={<Devolucao />}></Route>
+            <Route path="pagarMulta" element={<MultasDoLeitor/>}></Route>
           </Route>
         </Route>
 
